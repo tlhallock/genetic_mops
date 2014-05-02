cca35e8f026c39c67edca0caf12007ccc65eb086
tlhallock <theboxforspam@gmail.com>
2014-05-02 15:39:36 -0600

    should have been part of previous.

---
+++ Representer/src/backtracker.cc
@@ -0,0 +1,18 @@
+/*
+ * backtracker.cc
+ *
+ *  Created on: Apr 28, 2014
+ *      Author: rever
+ */
+//
+//void backtracker(InitialSet *set, double (*represent_metric)(InitialSet *set, char *to_use, char *to_represent),
+//		char *out_mask, char *current_mask, int depth)
+//{
+//
+//}
+//
+//
+//void backtracker(InitialSet *set, double (*represent_metric)(InitialSet *set, char *to_use, char *to_represent), char *out_mask)
+//{
+//
+//}
